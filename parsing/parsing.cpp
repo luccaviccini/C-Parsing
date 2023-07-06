f0551b696b2adf81408b43f63439aa93103fcef4
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-07-06 14:15:03 -0300

    feat: adding pu_list_latome function

--- parsing/parsing.cpp
+++ parsing/parsing.cpp
@@ -51,17 +51,70 @@ vector<string> processFile(const string& fileUrl) {
     return elementsArray;
 }
 
+vector<string> process_pu_list_latome(const string& fileUrl) {
+    ifstream file(fileUrl);
+
+    if (!file) {
+        cerr << "Failed to open the file: " << fileUrl << endl;
+        return vector<string>();
+    }
+
+    regex linePattern(R"(PU(\d+)\s+([\w_]+|GND)(?:\s+\(.*?\))?\s+([\w_]+|GND)(?:\s+\(.*?\))?\s+([\w_]+|GND)(?:\s+\(.*?\))?\s+([\w_]+|GND)(?:\s+\(.*?\))?\s+([\w_]+|GND)(?:\s+\(.*?\))?\s+([\w_]+|GND)(?:\s+\(.*?\))?)");
+    smatch matches;
+    vector<string> elementsArray;
+    string line;
+    int expectedPuNumber = 1;
+
+    while (getline(file, line)) {
+        if (regex_search(line, matches, linePattern)) {
+            int currentPuNumber = stoi(matches[1].str());
+
+            while (currentPuNumber > expectedPuNumber) {
+                for (int i = 0; i < 6; i++) {
+                    elementsArray.push_back("");
+                }
+                expectedPuNumber++;
+            }
+
+            for (size_t i = 2; i < matches.size(); i++) {
+                elementsArray.push_back(matches[i].str());
+            }
+
+            expectedPuNumber++;
+        }
+    }
+
+    // Fill the remaining elements with empty strings if the file ended before PU62
+    while (expectedPuNumber <= 62) {
+        for (int i = 0; i < 6; i++) {
+            elementsArray.push_back("");
+        }
+        expectedPuNumber++;
+    }
+
+    file.close();
+
+    return elementsArray;
+}
+
+
 
 int main() {
     const string baseDir = "../assets/"; // Change the directory accordingly
+    vector<string> fileNamesForProcessFile = {
+        "EMBA_1" // Add the file names you want to process with the processFile function
+    };
+    
+    vector<string> fileNamesForProcessPUList = {
+        "pu_list_latome" // Add the file names you want to process with the process_pu_list_latome function
+    };
 
-    // Processing IN data
-    vector<string> fileNamesIn = {"EMBA_1"}; // Change the filename accordingly
-    for (const string& fileName : fileNamesIn) {
+    // Process files using processFile function
+    for (const string& fileName : fileNamesForProcessFile) {
         string fileUrl = baseDir + fileName + ".txt";
         vector<string> elementsArray = processFile(fileUrl);
 
-        cout << "Processing file: " << fileName << ".txt" << endl;
+        cout << "Processing file with processFile: " << fileName << ".txt" << endl;
 
         size_t inNumber = 1;
         for (size_t i = 0; i < elementsArray.size(); i++) {
@@ -74,6 +127,24 @@ int main() {
         cout << "Size of elementsArray: " << elementsArray.size() << endl << endl;
     }
 
-    
+    // Process files using process_pu_list_latome function
+    for (const string& fileName : fileNamesForProcessPUList) {
+        string fileUrl = baseDir + fileName + ".txt";
+        vector<string> elementsArray = process_pu_list_latome(fileUrl);
+
+        cout << "Processing file with process_pu_list_latome: " << fileName << ".txt" << endl;
+
+        size_t puNumber = 1;
+        for (size_t i = 0; i < elementsArray.size(); i++) {
+            cout << "PU" << puNumber << " Element: " << elementsArray[i] << endl;
+            if ((i + 1) % 6 == 0) {
+                puNumber++;
+            }
+        }
+
+        cout << "Size of elementsArray: " << elementsArray.size() << endl << endl;
+    }
+
     return 0;
 }
+
